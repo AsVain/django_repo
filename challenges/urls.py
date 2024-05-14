9a7b33f76bccffcd1ce658424e68f2bfed7f9277
AsVain <przemorek11@gmail.com>
2024-05-14 09:16:13 +0200

    pliki bez komentarzy

--- challenges/urls.py
+++ challenges/urls.py
@@ -3,24 +3,8 @@ from django.urls import path    #funkcja przyjmuje dwa argumenty: string - URL o
 from . import views     #z tego samego folderu, gdzie jestem, użyj pliku views
 
 urlpatterns = [
-    #path("january", views.january),      #całe to wyrażenie tworzy tzw. URLconfig (URLConf)
-    #path("february", views.february),
-    #path("march", views.march),
+
     path("<int:month>", views.monthly_challenge_by_number),
     path("<str:month>", views.monthly_challenge)  # <> - placeholder         #nie obchodzi mnie co tu jest napisane, wiem, że jest to miesiac, moze to byc kazdy miesiac, path("<month>", views.monthly_challenge)
     
 ]
-#palceholder przyjmuje każde wartości, dlatego tak zsotawiony placeholder będzie przyjmował np słowa anything, koło, szkoła, wszystko...
-#Można ograniczyć ten placeholder, np. poprzez typ danych, jakim musi być. path("<str:month>", views.monthly_challenge)
-
-#mam URL, które są obsługiwane przez aplikację challenges w całym projekcie monthly_challenges.
-#trzeba teraz połączyć aplikacje challenge z całym projektem
-#Należy stworzyć URLconfig projektu, oprócz tego URLConfigu, który jest tutaj w tyej aplikacji
-
-
-#Można tak dodawać mnóstwo tych urli manualnie, ale można też automatycznie.
-
-#Jeśli wiem ile będzie URLi, można stworzyć URLPattern, który będzie dynamicznie przekazywał wartości. Jest View Funkcja, która skupia się na dynamicznych URLach.
-
-
-#Jeśli nie wiem ile będzie URLi 
--- challenges/views.py
+++ challenges/views.py
@@ -16,67 +16,9 @@ monthly_challenges = {                  #słownik z miesiącami, po to by zautom
     "november": "Walk for at least 20 minutes every day!",
     "december": "Eat no meat for the entire month!"
 }
-# Create your views here.
 
-#Pierwszy widok, na request do serwera, zwracam stringa. Póki co nie robię nic z requestem
-"""def january(request):
-    return HttpResponse("Eat no meat for the entire month!")
-
-def february(request):
-    return HttpResponse("Walk for at least 20 minutes every day!")
-
-#kiedy mamy to wywołać? Musimy to zdefininiować w urls.py, który stworzyłem. Mamy też folder urls.py w katalogu głównym projektu "monthly challenges", 
-#                                                                   ale musimy mieć zdefiniowany URL w pliku urls.py w katalogu aplikacji, jaką tworzę
-
-def march(request):
-    return HttpResponse("Learn Django for at least 20 minutes every day!")
-"""
 def monthly_challenge(requeste, month):
-    
-    """
-    challange_text = None   #by default będzie przyjmować wartość None
-   
-    if month == 'january':
-        challange_text = "Eat no meat for the entire month!"
-    
-    elif month == 'february':
-        challange_text = "Walk for at least 20 minutes every day!"
-        
-    elif month == 'march':
-        challange_text = "Learn Django for at least 20 minutes every day!"
-        
-    elif month == 'april':
-        challange_text = "Walk for a"
-        
-    elif month == 'may':
-        challange_text = " every day!"
-        
-    elif month == 'june':
-        challange_text = "20 minutes every day!"
-        
-    elif month == 'july':
-        challange_text = "Walk  20 minutes every day!"
-        
-    elif month == 'august':
-        challange_text = "Walk for at least 20 minutes day!"
-        
-    elif month == 'september':
-        challange_text = "Walk for at least 20 minutes every day"
-        
-    elif month == 'october':
-        challange_text = "Walk for at  20 minutes every day!"
-        
-    elif month == 'november':
-        challange_text = "Walk for at least  minutes every day!"
-        
-    elif month == 'december':
-        challange_text = " for at least 20 minutes every !"
-      
-    else:
-        return HttpResponseNotFound("This month is not supported!")   
 
-    return HttpResponse(challange_text)
-"""
     try: 
         challenge_text = monthly_challenges[month]
         return HttpResponse(challenge_text)
