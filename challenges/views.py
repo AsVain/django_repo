e9a627bce65b163512d351397ad8e5f60301ee10
AsVain <przemorek11@gmail.com>
2024-05-14 09:14:57 +0200

    pliki z komentarzami

--- challenges/views.py
+++ challenges/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound #wbudowana klasa #not found - 404 error moze byc wyswietlony
 
+
+monthly_challenges = {                  #słownik z miesiącami, po to by zautomatyzować proces i skrócić działanie kodu
+    "january": "Eat no meat for the entire month!",
+    "february": "Walk for at least 20 minutes every day!",
+    "march": "Learn Django for at least 20 minutes every day!",
+    "april": "Eat no meat for the entire month!",
+    "may": "Walk for at least 20 minutes every day!",
+    "june": "Eat no meat for the entire month!",
+    "july": "Learn Django for at least 20 minutes every day!",
+    "august": "Walk for at least 20 minutes every day!",
+    "september": "Eat no meat for the entire month!",
+    "october": "Learn Django for at least 20 minutes every day!",
+    "november": "Walk for at least 20 minutes every day!",
+    "december": "Eat no meat for the entire month!"
+}
 # Create your views here.
 
 #Pierwszy widok, na request do serwera, zwracam stringa. Póki co nie robię nic z requestem
@@ -18,8 +33,9 @@ def march(request):
 """
 def monthly_challenge(requeste, month):
     
+    """
     challange_text = None   #by default będzie przyjmować wartość None
-    
+   
     if month == 'january':
         challange_text = "Eat no meat for the entire month!"
     
@@ -55,12 +71,21 @@ def monthly_challenge(requeste, month):
         
     elif month == 'december':
         challange_text = " for at least 20 minutes every !"
-        
+      
     else:
         return HttpResponseNotFound("This month is not supported!")   
+
     return HttpResponse(challange_text)
+"""
+    try: 
+        challenge_text = monthly_challenges[month]
+        return HttpResponse(challenge_text)
+    except:
+        return HttpResponseNotFound("This month is not supported!")    
 
 def monthly_challenge_by_number(request, month):
+    months = monthly_challenges.keys()
+    
     return HttpResponse(month)
 
 
