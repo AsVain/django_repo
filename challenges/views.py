a45358de610f67b86f8554a423baacc17b2f5b4f
AsVain <przemorek11@gmail.com>
2024-05-15 10:26:07 +0200

    render() zamiast render_to_string()

--- challenges/views.py
+++ challenges/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render #zamienia funkcję render_to_string i wysyła z powrotem odpowiedź. 
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect #wbudowana klasa #not found - 404 error moze byc wyswietlony oraz klasa do przekierowywania
 from django.urls import reverse #pozwala tworzyć ścieżki (paths) poprzez odsyłanie do nazw ścieżek tych URL
-from django.template.loader import render_to_string #generowanie czegoś do stringa
+#from django.template.loader import render_to_string #generowanie czegoś do stringa
 
 
 monthly_challenges = {                  #słownik z miesiącami, po to by zautomatyzować proces i skrócić działanie kodu
@@ -34,9 +34,10 @@ def index(reqest):      #lista miesięcy na stronie głównej
 
 def monthly_challenge(request, month):      #przyjmuje request od klienta oraz month (placeholder obojetnie jakiego URL)
     try: 
-        challenge_text = monthly_challenges[month]
-        response_data = render_to_string("challenges/challenge.html")                                #f"<h1>{challenge_text}</h1>"    #f-string -> dzieki temu mozna zawrzec miedzy cudzyslowiem wartosci, zmienne jakie chcemy
-        return HttpResponse(response_data)             #co zwraca użytkownikowi
+        challenge_text = monthly_challenges[month]  
+        return render(request, "challenges/challenge.html") #render() potrzebuje 2 argumentow, request oraz sciezka do pliku
+        #response_data = render_to_string("challenges/challenge.html")                                #f"<h1>{challenge_text}</h1>"    #f-string -> dzieki temu mozna zawrzec miedzy cudzyslowiem wartosci, zmienne jakie chcemy
+        #return HttpResponse(response_data)             #co zwraca użytkownikowi
     except:
         return HttpResponseNotFound("<h1>This month is not supported!</h1>")    
 
