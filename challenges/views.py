22c4cb6cb174501e65a88dd2b8332c6f6bffab03
AsVain <przemorek11@gmail.com>
2024-05-15 08:16:12 +0200

    uporzdkowanie kodu

--- challenges/views.py
+++ challenges/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect #wbudowana klasa #not found - 404 error moze byc wyswietlony oraz klasa do przekierowywania
 from django.urls import reverse #pozwala tworzyć ścieżki (paths) poprzez odsyłanie do nazw ścieżek tych URL
 
+
 monthly_challenges = {                  #słownik z miesiącami, po to by zautomatyzować proces i skrócić działanie kodu
     "january": "Eat no meat for the entire month!",
     "february": "Walk for at least 20 minutes every day!",
@@ -17,7 +18,7 @@ monthly_challenges = {                  #słownik z miesiącami, po to by zautom
     "december": "Eat no meat for the entire month!"
 }
 
-def index(reqest):
+def index(reqest):      #lista miesięcy na stronie głównej
     list_items = ""
     months = list(monthly_challenges.keys())
     
@@ -29,6 +30,7 @@ def index(reqest):
     response_data = f"<ul>{list_items}</ul>"   
     return HttpResponse(response_data)
 
+
 def monthly_challenge(request, month):      #przyjmuje request od klienta oraz month (placeholder obojetnie jakiego URL)
     try: 
         challenge_text = monthly_challenges[month]
@@ -36,6 +38,7 @@ def monthly_challenge(request, month):      #przyjmuje request od klienta oraz m
         return HttpResponse(response_data)             #co zwraca użytkownikowi
     except:
         return HttpResponseNotFound("<h1>This month is not supported!</h1>")    
+
 
 def monthly_challenge_by_number(request, month):        #redirections - numery na miesiące
     months = list(monthly_challenges.keys())                #.keys() - funkcja, która wypisuje keys ze słownika
@@ -47,7 +50,6 @@ def monthly_challenge_by_number(request, month):        #redirections - numery n
     redirect_path = reverse("month-challenge", args=[redirect_month])          #/challenge/january      #dzieki temu mozemy zmienic url w projekcie jak i kiedy chcemy
     return HttpResponseRedirect(redirect_path)    #challanges są zahardkodowane w urlach całego projektu, nie chcemy tego.
 
-    #response with code 3## to redirect 
     """
         responses:
         1 - informational responses
