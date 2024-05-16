76df7bcacf0ca24a3d42bb76617f8586210379e8
AsVain <przemorek11@gmail.com>
2024-05-16 13:35:48 +0200

    css menu

--- monthly_challenges/settings.py
+++ monthly_challenges/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles',   #potrzebne, żeby działały pliki css (i statyczne pliki ogólnie)
     'challenges',       #nazwa aplikacji, odnajdziemy ją np. w folderze apps.py w folderze aplikacji
                         #Jeśli jest aplikacja, która jest częścią projektu, należy ją oficjalnie zarejestrować w projekcie: settings .py -> installed_apps. 
                         #Wtedy dzięki app_dirs, będzie automatycznie wyszukiwany folder z templatkami
@@ -121,9 +121,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'  #django będzie szukac w folderach static, statycznych plikow. W folderach aplikacji bedzie suzkac
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
